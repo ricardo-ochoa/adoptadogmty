ffbdc002a4045081dae480f5ee089f2ef5c2ba6d
Ochoagram <ricardogochoa@me.com>
2024-09-14 01:56:36 -0600

    new sections

--- src/app/requisitos/page.tsx
+++ src/app/requisitos/page.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import 'animate.css';
 import { useState } from 'react';
-import ShareModal from './ShareModal';
 
 interface LinkButtonProps {
     href: string;
